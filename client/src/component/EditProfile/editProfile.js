54914cc3f755e97b80a01ab279e494bffea0fe9d
ylwn29 <ylwn29@gmail.com>
2022-12-10 23:20:05 -0800

    upload edit profile screen

---
+++ client/src/component/EditProfile/editProfile.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react'
+import UserCardEditProfile from './userCardEditProfile'
+import EditProfileItem from './editProfileItem'
+import './editProfile.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { getUserProfile } from '../../actions/userActions'
+
+const EditProfile = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const userProfile = useSelector(state => state.userProfile)
+    const { user } = userProfile
+
+
+    useEffect(() => {
+        if (!userInfo) {
+            navigate('/login')
+        } else if (!user.name) {
+            dispatch(getUserProfile())
+        }
+    }, [dispatch, userInfo, user, navigate])
+
+    return (
+        <>
+            <div>
+
+                <section className="header height-auto">
+                    <div className="row height-50vh">
+                        <div className="col-4">
+                            <UserCardEditProfile profile={user}/>
+                        </div>
+                        <div className="col-8">
+                            <EditProfileItem profile={user}/>
+                        </div>
+                    </div>
+                </section>
+                <section className="homepage-card-section">
+                </section>
+            </div>
+        </>
+    )
+}
+export default EditProfile
---
+++ client/src/component/EditProfile/userCardEditProfile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './editProfile.css'
+import LetterAvatars from './letterAvatars'
+
+const UserCardEditProfile = ({ profile }) => {
+
+    return (
+        <>
+            <div className="user-card user-card-flex">
+                <div className="m-5">
+                    <div className="avatar-position">
+                        <LetterAvatars name={profile.name}/>
+                    </div>
+                    <h3 className="user-card-name text-black"><span>{profile.firstname}</span> <span>{profile.lastname}</span>
+                    </h3>
+                    <p className="text-black">{profile.bio}</p>
+
+                    <Link className="btn rounded-pill bg-success fw-bold mt-1" to="/profile">Back</Link>
+                </div>
+            </div>
+        </>
+    )
+}
+export default UserCardEditProfile
